9dff34231172177923175a7bccb8f9af7b155b9b
sinansubasi95 <sinan95@gmail.com>
2021-01-24 16:06:26 +0300

    create user schema

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
+// graphqlHTTP is middleware
 // graphiql is development tool that allows us to make queries against our development server so only intented to be use in development envrionment
 app.use('/graphql', graphqlHTTP({
+    schema,
     graphiql: true
 }));
 
