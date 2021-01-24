3403b1c1485bc409dd66fbab81625f4ae9a8f4c4
sinansubasi95 <sinan95@gmail.com>
2021-01-24 13:33:56 +0300

    register graphql with express

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
+
+// graphiql is development tool that allows us to make queries against our development server so only intented to be use in development envrionment
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}));
 
 app.listen(4000, () => {
     console.log('Listening');
